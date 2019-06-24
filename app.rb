878efb3f9d33854796452f0f0d5b57f906500e4a
Jim Hough <teachlife@outlook.com>
2019-06-24 01:31:19 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,6 +7,13 @@ class App < Sinatra::Base
   end
 
   post '/team' do
+    @name = params[:name]
+    @coach = params[:coach]
+    @sg = params[:sg]
+    @pg = params[:pg]
+    @sf = params[:sf]
+    @pf = params[:pf]
+    @c = params[:c]
     erb :team
   end
 end
